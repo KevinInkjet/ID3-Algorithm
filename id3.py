7c5dfd1e41cb287b9ec59f601dd4563fadbf3b6f
Kevin Cárdenas <55776808+KevinInkjet@users.noreply.github.com>
2021-09-03 11:19:54 -0700

    Día 2
    Algunas funciones para la manipulación de archivos.

--- id3.py
+++ id3.py
@@ -1,17 +1,20 @@
-archivo = open("C:/Users/Owner/Documents/LCC/Machine Learning/ID3-Algorithm/prueba.txt")
+archivo = open("C:/Users/Owner/Documents/LCC/Machine Learning/ID3-Algorithm/prueba.txt") # Abrir un archivo
 
-linea = archivo.readline()
-while linea != '':
-    print(linea)
-    linea = archivo.readline()
-
+total_lines = sum(1 for line in archivo)
+print("Líneas del archivo: ", total_lines) #Para conseguir la cantidad de líneas del archivo
 archivo.close()
 
-archivo = open("C:/Users/Owner/Documents/LCC/Machine Learning/ID3-Algorithm/prueba.txt")
+archivo = open("C:/Users/Owner/Documents/LCC/Machine Learning/ID3-Algorithm/prueba.txt") # Abrir un archivo
 
-linea = archivo.read()
-while linea != '':
-    print(linea)
-    linea = archivo.read()
+datos = []
+for lineas in archivo:
+    datos.append(lineas.split())
 
-archivo.close()
+print(datos)
+
+print(len(datos[2])) #Con len() obtenemos la longitud de una línea, en este caso la línea 3
+
+palabra = "hola,"
+print(palabra)
+palabra = palabra.replace(",", '') #Quitamos las comas
+print(palabra)
