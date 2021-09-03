06f03363b7e0a66a27088d605b71abb8e8944475
Kevin Cárdenas <55776808+KevinInkjet@users.noreply.github.com>
2021-09-02 22:48:07 -0700

    Mal comienzo

--- id3.py
+++ id3.py
@@ -1,2 +1,17 @@
-print("Hola")
 archivo = open("C:/Users/Owner/Documents/LCC/Machine Learning/ID3-Algorithm/prueba.txt")
+
+linea = archivo.readline()
+while linea != '':
+    print(linea)
+    linea = archivo.readline()
+
+archivo.close()
+
+archivo = open("C:/Users/Owner/Documents/LCC/Machine Learning/ID3-Algorithm/prueba.txt")
+
+linea = archivo.read()
+while linea != '':
+    print(linea)
+    linea = archivo.read()
+
+archivo.close()
